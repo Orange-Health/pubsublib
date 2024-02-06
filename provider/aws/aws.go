dbbad59f8ea0cc3cf389c5b897fd7a4493fa2225
gmmanu <manu.gm@orangehealth.in>
2024-02-06 16:13:51 +0530

    made publish func generic for normal and FIFO topic

--- provider/aws/aws.go
+++ provider/aws/aws.go
@@ -59,68 +59,12 @@ func NewAWSPubSubAdapter(region, accessKeyId, secretAccessKey, redisAddress, red
 Publishes the message with the messageAttributes to the topicARN provided.
 source, contains and eventType are necessary keys in messageAttributes.
 Returns error if fails to publish message
+
+When the SNS Topic is FIFO type, messageGroupId and messageDeduplicationId are required.
+- messageGroupId : SNS orders the messages in a message group into a sequence.
+- messageDeduplicationId : SNS uses this to determine whether to create a new message or to use an existing one.
 */
-func (ps *AWSPubSubAdapter) Publish(topicARN string, message interface{}, messageAttributes map[string]interface{}) error {
-	// Check if message is of type map[string]interface{} and then convert all the keys to snake_case
-	switch message.(type) {
-	case map[string]interface{}:
-		message = helper.ConvertBodyToSnakeCase(message.(map[string]interface{}))
-	}
-
-	jsonString, err := json.Marshal(message)
-	if err != nil {
-		return err
-	}
-
-	// figure out the message body as required
-	messageBody := string(jsonString)
-	if len(messageBody) > 200*1024 {
-		// body is larger than 200kB. Best to put it in redis with expiry time of 10 days
-		redisKey := uuid.New().String()
-		messageAttributes["redis_key"] = redisKey
-
-		// Set the message body in redis db
-		err := ps.redisClient.Set(redisKey, messageBody, 10*24*60)
-		if err != nil {
-			return err
-		}
-
-		messageBody = "body is stored in redis under key PUBSUB:" + redisKey
-	}
-
-	if messageAttributes["source"] == nil {
-		return fmt.Errorf("should have source key in messageAttributes")
-	}
-	if messageAttributes["contains"] == nil {
-		return fmt.Errorf("should have contains key in messageAttributes")
-	}
-	if messageAttributes["event_type"] == nil {
-		return fmt.Errorf("should have event_type key in messageAttributes")
-	}
-	if messageAttributes["trace_id"] == nil {
-		messageAttributes["trace_id"] = uuid.New().String()
-	}
-	awsMessageAttributes := map[string]*sns.MessageAttributeValue{}
-	if messageAttributes != nil {
-		awsMessageAttributes, _ = BindAttributes(messageAttributes)
-	}
-	_, err = ps.snsSvc.Publish(&sns.PublishInput{
-		Message:           aws.String(messageBody), // Ensures to always send compressed message
-		TopicArn:          aws.String(topicARN),
-		MessageAttributes: awsMessageAttributes,
-	})
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-/*
-Publishes the message with the messageAttributes to the FIFO enabled topicARN provided.
-source, contains and eventType are necessary keys in messageAttributes.
-Returns error if fails to publish message
-*/
-func (ps *AWSPubSubAdapter) PublishFIFO(topicARN, messageGroupId, messageDeduplicationId string, message interface{}, messageAttributes map[string]interface{}) error {
+func (ps *AWSPubSubAdapter) Publish(topicARN string, messageGroupId, messageDeduplicationId string, message interface{}, messageAttributes map[string]interface{}) error {
 	// Check if message is of type map[string]interface{} and then convert all the keys to snake_case
 	switch message.(type) {
 	case map[string]interface{}:
